e8afb83fdff8be7a9a6564eba87610501a4c0394
Mezo <ahmedmaher.maher61@gmail.com>
2023-10-18 16:43:33 +0300

    Add project files.

---
+++ PhoneBook/PhoneBook.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhoneBook
+{
+    internal class PhoneBook
+    {
+        #region Attributes
+        private int size;
+        string[] names;
+        long[] phoneNumbers;
+        #endregion
+
+        #region Properties
+        public int Size { get { return size; } }
+        public long this[string Name] // searching and updating indexer(read and write)
+        {
+            get
+            {
+                if (names is not null & phoneNumbers is not null)
+                {
+                    for (int i = 0; i < names.Length; i++)
+                    {
+                        if (names[i] == Name)
+                            return phoneNumbers[i];
+                    }
+                }
+                return -1;
+            }
+            set
+            {
+                if(names is not null & phoneNumbers is not null)
+                {
+                    for(int i = 0; i < names.Length; i++)
+                    {
+                        if (names[i] == Name)
+                            phoneNumbers[i] = value;
+                    }
+                }
+            }
+        }
+
+        public string this[int Index]
+        {
+            get
+            {
+                if(names is not null & phoneNumbers is not null)
+                {
+                    if(Index >= 0 & Index < names.Length)
+                    {
+                        return $"Index: {Index}, Name: {names[Index]}, PhoneNumber: {phoneNumbers[Index]}";
+                    }
+                }
+                return "Invalid Index";
+            }
+        }
+        #endregion
+
+        #region Constructors
+        public PhoneBook(int _size)
+        {
+            size = _size;
+            names = new string[size];
+            phoneNumbers = new long[size];
+        }
+        #endregion
+
+        #region Methods
+        /// <summary>
+        /// AddNumber is a method to insert a new cell in the PhoneBook
+        /// </summary>
+        /// <param name="Index"></param>
+        /// <param name="Name"></param>
+        /// <param name="PhoneNumber"></param>
+        public void AddNumber(int Index, string Name, long PhoneNumber)
+        {
+            if (names is not null & phoneNumbers is not null)
+            {
+                if (Index >= 0 & Index < size)
+                {
+
+                    names[Index] = Name;
+                    phoneNumbers[Index] = PhoneNumber;
+                }
+
+            }
+        }
+        #endregion
+    }
+}
---
+++ PhoneBook/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace PhoneBook
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            PhoneBook NoteBook = new PhoneBook(5);
+
+            NoteBook.AddNumber(0, "Ahmed", 11111152422);
+            NoteBook.AddNumber(1, "Mohammed", 11114512789);
+            NoteBook.AddNumber(2, "Omar", 1546480001);
+            NoteBook.AddNumber(3, "Hassan", 12585456365);
+            NoteBook.AddNumber(4, "Mostafa", 12545454554);
+
+            for (int i = 0; i < NoteBook.Size; i++)
+            {
+                Console.WriteLine(NoteBook[i]);
+            }
+            Console.WriteLine("---------------------------------------------------");
+            NoteBook["Omar"] = 1111111111;
+            //Console.WriteLine(NoteBook["Omar"]);
+
+            for(int i = 0; i < NoteBook.Size; i++)
+            {
+                Console.WriteLine(NoteBook[i]);
+            }
+        }
+    }
+}
